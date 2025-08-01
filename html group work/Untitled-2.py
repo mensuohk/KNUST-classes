dff9eaa594c889eea8310b7f12bf1f8cbbab562d
mensuohk <mensuohk@gmail.com>
2025-08-01 11:27:36 +0000

    Add initial implementations for number printing and course enrollment details

---
+++ html group work/Untitled-2.py
@@ -0,0 +1,5 @@
+for i in range(2,100,2):
+    print(i)
+print("Odd numbers from 1 to 99:")
+for i in range(1,100,2):
+    print(i)    
---
+++ html group work/Untitled-3.py
@@ -0,0 +1,13 @@
+dict = {"math":20, "science":30, "english":50}
+print(dict)
+for course in dict.keys():
+    print(dict[course],course)
+    print(f"{course} has {dict[course]} students enrolled.")
+
+list = [1, 2, 3, 4, 5, 3]
+print(list.count(3))
+
+i = [0]
+print(type(i))
+
+print(list)
